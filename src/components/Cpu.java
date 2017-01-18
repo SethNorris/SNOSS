c6d2b84a146ff04f48deac9340f2a743f9e4f6c9
Seth Norris <sethhnorris7@live.com>
2017-01-18 08:24:08 -0700

    CPU roots

---
+++ src/components/Cpu.java
@@ -0,0 +1,95 @@
+package components;
+
+public class Cpu implements Instructions{
+
+	byte[] Register0 = new byte[8];
+	byte[] Register1 = new byte[8];
+	byte[] Register2 = new byte[8];
+	byte[] Register3 = new byte[8];
+	byte[] Register4 = new byte[8];
+	byte[] Register5 = new byte[8];
+	
+	byte[] RAM = new byte[10000];
+
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loadc() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void store() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sub() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mul() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void div() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eq() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mygoto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gotoif() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cprint() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cread() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+}
---
+++ src/components/Instructions.java
@@ -0,0 +1,18 @@
+package components;
+
+public interface Instructions {
+
+	public void load();
+	public void loadc();
+	public void store();
+	public void add();
+	public void sub();
+	public void mul();
+	public void div();
+	public void eq();
+	public void mygoto();
+	public void gotoif();
+	public void cprint();
+	public void cread();
+	public void exit();
+}
